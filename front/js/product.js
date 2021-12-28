3810a4ebbdc51ed61eca00a842aa591f246d5c04
SONDESSLM <sondess.landolsi@laposte.net>
2021-12-28 13:14:35 +0100

    modification page index

--- front/js/product.js
+++ front/js/product.js
@@ -1,53 +1,39 @@
 
-var productId = new URL (window.location.href).searchParams.get("id");
-console.log(productId);
+var productId = new URL(window.location.href).searchParams.get("id");
 
-
-
-
-
-  
-  getOneProduct();
 /**
  * Get Oneproduct.
  * 
  * array one products from The product list.
  */
- async function getOneProduct() {
-  let product={};
-  let product_response= await fetch("http://localhost:3000/api/products/"+productId);
-  if (product_response.ok) 
-  {
-      product= await product_response.json();
-      
+async function getOneProduct() {
+  let product = {};
+  let product_response = await fetch("http://localhost:3000/api/products/" + productId);
+  if (product_response.ok) {
+    product = await product_response.json();
+
   }
   else {
-      product=console.error();
-      alert('le produit que vous chercher est indisponble pour le moment');
-      
+    alert('le produit que vous chercher est indisponble pour le moment');
+
   }
   return product;
 }
 
-displayOneProduct();
 /**
  * Display  product on the product.html page.
  */
- async function displayOneProduct(){
-  let product= await getOneProduct();
+async function displayOneProduct() {
+  let product = await getOneProduct();
   let item_selector = document.querySelector('.item');
-  const productColor = document.querySelector('#colors');
-  const productQuantity = document.querySelector ('#quantity');
-  async function productColors(){
-      let colors = product.colors;
-      console.table (colors);
-      let productColors = document.createElement ("value");
-      document.querySelector('#color').appendChild (productColors);
-      productColors.value = colors;
-      productColors.innerHTML = colors;
-     }
- 
-  item_selector.innerHTML =`<article> 
+  console.log(product, typeof product == "undefined" );
+  if (typeof product == "undefined" || product.length == 0) {
+    item_selector.innerHTML = `<p> le produit que vous chercher est indisponble pour le moment </p>`;
+  }
+  else {
+    const productQuantity = document.querySelector('#quantity');
+
+    item_selector.innerHTML = `<article> 
   <div class= "item__img" ${product._id}">
   <img src="${product.imageUrl}" alt="${product.altTxt}</div>
   <div class="item__content">
@@ -62,12 +48,7 @@ displayOneProduct();
   <div class="item__content__settings">
     <div class="item__content__settings__color">
       <label for="color-select">Choisir une couleur :</label>
-      <select name="color-select" id="colors">
-          <option value="">--SVP, choisissez une couleur --</option>
-          <option value= "">${product.colors}</option>
-          <option value="">${product.colors}</option>
-          <option value="">${product.colors}</option>
-      </select>
+      <select name="color-select" id="colors"></select>
     </div>
     <div class="item__content__settings__quantity">
       <label for="itemQuantity">Nombre d'article(s) (1-100) :</label>
@@ -78,10 +59,14 @@ displayOneProduct();
     <button id="addToCart">Ajouter au panier</button>
   </div>
  </div>   
-</article>`; 
+</article>`;
 
+    let productColor = document.querySelector('#colors');
+    let colors = product.colors;
+    colors.forEach(function (color) {
+      productColor.innerHTML += `<option value="${color}">${color}</option>`;
+    });
+  }
 };
- 
-   
-  
+
 displayOneProduct();
--- front/js/script.js
+++ front/js/script.js
@@ -1,43 +1,43 @@
+getProducts();
+
 /**
  * Get products.
  * 
- * @returns array products The product list.
  */
 async function getProducts() {
-    let products={};
-    let products_response= await fetch("http://localhost:3000/api/products");
-    if (products_response.ok) 
-    {
-        products= await products_response.json();
-        
-    }
-    else {
-       
-       alert('Les produits ne sont pas disponibles pour le moment');
-      
-    }
-    return products;
-    
+    await fetch('http://localhost:3000/api/products')
+        .then((response) => response.json())
+        .then((data) => {
+            displayProducts(data);
+        })
+        .catch((erreur) => {
+            console.log('erreur :' + erreur);
+        });
 }
 
 /**
  * Display products on the index.html page.
  */
-async function displayProducts(){
-    let products= await getProducts();
+async function displayProducts(products) {
     let items_selector = document.querySelector('.items');
-    products.forEach(function (product) {
-    items_selector.innerHTML +=`<a href="./product.html?id=${product._id}">
+    if (!isEmpty(products)) {
+        products.forEach(function (product) {
+            items_selector.innerHTML += `<a href="./product.html?id=${product._id}">
         <article>
         <img src="${product.imageUrl}" alt="${product.altTxt}">
         <h3 class="productName">${product.name}</h3>
         <p class="productDescription">${product.description}</p>
         </article>
         </a>`;
-       
-});
+
+        });
+    } else {
+        items_selector.innerHTML += `<p>Aucun produit n'est disponble pour le moment.</p>`;
+    }
 
 }
 
 
-displayProducts();
+function isEmpty(value) {
+    return typeof value == 'string' && !value.trim() || typeof value == 'undefined' || value === null;
+}
